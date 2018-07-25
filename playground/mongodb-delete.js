b81f7ea4624e07a167aa3f1734300a80ec817a07
FieryKnight_91 <punyashlok.com@gmail.com>
2018-07-24 18:49:10 -0700

    delete file added

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,40 @@
+/*
+
+var user = {Name:'Puplu', Age:27};
+var {Name} = user;  //Object destructuring to use values of fields for creating variables
+console.log(Name);
+
+*/
+
+
+//const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb'); // Same as above: Uses destructuring to fit value of 'MongoClient' from mongodb obj to the const MongoClient
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, client) => {
+    if(err)
+    {
+       return console.log('Unable to connect to mongodb server');
+    }
+    console.log('Connected to mongodb server');
+
+    const db = client.db('TodoApp');
+
+    //delete Many
+    // db.collection('Todos').deleteMany({text:'Eat dinner'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // delete One
+    // db.collection('Todos').deleteOne({text:'Walk the dog'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Todos').findOneAndDelete({completed:false}).then((result) => {
+        console.log(result);
+    });
+
+    //find One & delete
+
+    client.close();
+});
