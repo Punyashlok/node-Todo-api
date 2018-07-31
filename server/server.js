fa5f489a36f9fdd4ff99fdad2d838351bdcc559e
FieryKnight_91 <punyashlok.com@gmail.com>
2018-07-30 18:02:51 -0700

    Changed server.js to include req, res

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-var {mongoose} = require('./db/mongoose.js');
+var express = require('express');
+var bodyParser = require('body-parser');
 
+var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {user} = require('./models/user.js');
 
@@ -14,4 +16,29 @@ item1.save().then((doc) => {
     console.log('Unable to save item',err);
 });
  */
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+
+    var todo = new Todo ({
+        text: req.body.text,
+        completed:req.body.completed
+    });
+    console.log(req.body);
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+
+
+app.listen('4000', () => {
+    console.log('Started on port: 4000');
+});
 
